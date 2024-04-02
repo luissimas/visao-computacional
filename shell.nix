24213c96a1f9f85d797f53c4b33cd8b4dc2fa27c
Luís Simas <luissimas@protonmail.com>
2024-04-02 07:19:47 -0300

    feat: micromamba setup

--- shell.nix
+++ shell.nix
@@ -2,4 +2,11 @@
 
 with pkgs;
 
-mkShell { buildInputs = [ python3 conda jupyter ]; }
+mkShell {
+  buildInputs = [ python3 micromamba jupyter python312Packages.pip ];
+
+  shellHook = ''
+    eval "$(micromamba shell hook --shell bash)"
+    micromamba activate visao_computacional
+  '';
+}
